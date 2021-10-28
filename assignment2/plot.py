f2721f895423ec56219858bdcde3210cb1acfca7
berkeleybear671 <berkeleybear671@gmail.com>
2021-10-28 16:29:01 -0400

    Update plot.py
    with LD and LD PI intensity

--- assignment2/plot.py
+++ assignment2/plot.py
@@ -28,12 +28,53 @@ for idx in range(len(cellLine)):
     # print(toxin_conc)
     plt.plot(toxin_conc, record)
     
-    plt.title('LD50 Curve for ' + name[idx])
+    plt.title('LD50 Curve for PI Intensity of ' + name[idx])
     plt.xlabel('Toxin Concentration (mg/mL)')
     plt.ylabel('Cell Death (%)')
     plt.ylim([0,0.6])
     plt.xscale("log")
     plt.tight_layout()
     plt.show()
+
+# %%
+# counts by well for cell line
+wells_c1 = continuous1.wellCounts()
+wells_c2 = continuous2.wellCounts()
+wells_w3 = wildtype3.wellCounts()
+wells_b3 = blast3.wellCounts()
+wells_c1.head()
+# %%
+#This plot takes the total live in each well in each dilution column / total alive in neg control
+#to give % cells alive
+
+cellLine = [wells_c1, wells_c2, wells_w3, wells_b3]
+name = ["countinuous1", "continuous2", "wildtype", "blast"]
+for idx in range(len(cellLine)):
+    record = []
+    neg_alive = np.mean(cellLine[idx][cellLine[idx].contents == "Negative"]["cell"])
+    toxin_conc = sorted((cellLine[idx])[(cellLine[idx])["contents"] == "Treatment"]["dilution"].unique())
+    for dilution in toxin_conc:
+        alive_no_at_dil = (cellLine[idx])[(cellLine[idx])["dilution"] == dilution]
+        
+        alive_no_avg = np.mean(alive_no_at_dil["cell"])
+        alive_ratio = alive_no_avg /neg_alive
+        record.append(alive_ratio)
+    # print(record)
+    # print("\n")
+    # print(toxin_conc)
+    
+    plt.plot(toxin_conc, record)
+    # print(toxin_conc)
+    # print("\n")
+    # print(record)
+    
+    plt.title('LD50 Curve for ' + name[idx])
+    plt.xlabel('Toxin Concentration (mg/mL)')
+    plt.ylabel('Cell Death (percent live/expected alive)')
+    #plt.ylim([0,0.6])
+    plt.xscale("log")
+    plt.tight_layout()
+    plt.show()
+    # %%
 
 # %%
